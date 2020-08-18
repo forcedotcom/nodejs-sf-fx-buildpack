46021a6eb283701065c5b2bffe0d22f4410e34d7
Josh W Lewis <josh.w.lewis@gmail.com>
2020-08-18 13:14:55 -0500

    Adjust function importer to rely on argument instead of ENV

--- middleware/userFnLoader.ts
+++ middleware/userFnLoader.ts
@@ -1,16 +1,14 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-export default function loadUserFunction(): any {
-  const packageName = process.env["SF_FUNCTION_PACKAGE_NAME"];
+export default function loadUserFunction(packageName: string): any {
   if (!packageName) {
-    console.dir(process.env);
-    throw `Could not locate function module, $SF_FUNCTION_PACKAGE_NAME not defined.`;
+    throw `Could not locate salesforce function module: package name not defined.`;
   }
   let mod;
   try {
     mod = require(packageName);
   } catch (err) {
-    throw `Could not locate function module: ${err}`;
+    throw `Could not locate salesforce function module: ${err}`;
   }
   if (mod.__esModule && typeof mod.default === 'function') {
     return mod.default;
