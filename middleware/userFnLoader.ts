6d7c5ed388dcb4b90c15cdb5f6f64b3d1a5d635a
Josh W Lewis <josh.w.lewis@gmail.com>
2020-08-18 13:03:18 -0500

    Change env var name to be salesforce functions specific

--- middleware/userFnLoader.ts
+++ middleware/userFnLoader.ts
@@ -1,10 +1,10 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 export default function loadUserFunction(): any {
-  const packageName = process.env["USER_FUNCTION_PACKAGE_NAME"];
+  const packageName = process.env["SF_FUNCTION_PACKAGE_NAME"];
   if (!packageName) {
     console.dir(process.env);
-    throw `Could not locate function module, $USER_FUNCTION_PACKAGE_NAME not defined.`;
+    throw `Could not locate function module, $SF_FUNCTION_PACKAGE_NAME not defined.`;
   }
   let mod;
   try {
